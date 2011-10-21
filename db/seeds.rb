199b55376e68b77af1b51656ec5a567ffa44613c
tim-tang <tang.jilong@139.com>
2011-10-21 16:51:13 +0800

    modify prepare data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,3 @@
 #   rake db:seed
 EydUser.create(:name=>"lisa.dong", :password=>"pas$word123", :password_confirmation=>"pas$word123")
 EydUser.create(:name=>"tim.tang", :password=>"tim83tang", :password_confirmation=>"tim83tang")
-
-EydConstant.create(:id=>1,:category => 'YoYo blog', :user_id=>"1")
-EydConstant.create(:id=>2,:category => 'Tim technic blog', :user_id=>"2")
-
-
-#EydComment.create(:id=>1, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>2, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>3, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>4, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>5, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>6, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>7, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>8, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>9, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>10, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-#EydComment.create(:id=>11, :parent_id => 0, :blog_id=>3, :book_id=>5, :name=>"tim.tang", :email=>"tang.jilong@gmail.com", :content=>"testing for comment!", :is_guestbook=>false)
-
