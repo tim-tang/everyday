e2e21ae25489f36424df79ef8b9f5631d807a27f
tim-tang <tang.jilong@139.com>
2011-10-29 13:22:46 +0800

    use gavatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,13 @@ module ApplicationHelper
     end  
     return text
   end
+
+  def avatar_url(comment)
+    if comment.email.present?
+    gravatar_id = Digest::MD5.hexdigest(comment.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    else
+      "http://www.everyday-cn.com/images/avatar.png"
+    end
+  end
 end
